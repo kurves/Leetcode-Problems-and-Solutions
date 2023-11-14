1724c2aeb06660e820b20a6d4d382c3339e8b780
kurves <kavssimply@gmail.com>
2023-11-14 21:44:06 +0300

    get sum of arr

--- twosum.py
+++ twosum.py
@@ -1,9 +1,10 @@
 def twosum(arr,n):
-    for i in arr:
-        print(arr[i])
-    s = sum(arr);
-    if s == n:
-        print (True)
+    for i,a in enumerate(arr):
+        if sum(arr) == n:
+            print(a)
+            print(i)
+    
+   
     print(s)
 
 twosum([3,4,6],13)        
