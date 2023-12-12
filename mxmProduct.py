8a49e387667ebe0e785641d5ece94e437ce6126f
kurves <kavssimply@gmail.com>
2023-12-12 23:17:24 +0300

    feat : get the multulication

--- mxmProduct.py
+++ mxmProduct.py
@@ -1,9 +1,11 @@
 def mxmProduct(nums):
-    k = sorted(nums)
-    return k
+    k = [i for i in sorted(nums)]
+    print(k) 
     i=k[-1]
     j=k[-2]
     print(i,j)
+    p = (i-1) * (j-1)
+    print(p)
 
 
 mxmProduct([2,1,6,7,3])
