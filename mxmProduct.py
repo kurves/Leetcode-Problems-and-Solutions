5ddf579a16213a2e6a11bae13b7ba9fffcb13e3d
kurves <kavssimply@gmail.com>
2023-12-12 22:55:12 +0300

    get the the last and second llast

--- mxmProduct.py
+++ mxmProduct.py
@@ -1,8 +1,9 @@
 def mxmProduct(nums):
-    for i in sorted(nums):
-        i=nums[-1]
-        j=nums[-2]
-        print(i,j)
+    k = sorted(nums)
+    return k
+    i=k[-1]
+    j=k[-2]
+    print(i,j)
 
 
 mxmProduct([2,1,6,7,3])
