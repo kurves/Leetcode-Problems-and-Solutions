007b6ec19984f2432242628ab1a9114cc9ed29cf
kurves <kavssimply@gmail.com>
2023-12-13 22:00:51 +0300

    get the product

--- mxmProduct.py
+++ mxmProduct.py
@@ -1,4 +1,6 @@
 def mxmProduct(nums):
+    nums.sort()
+        return (nums[-1] - 1) * (nums[-2] - 1)
     k = [i for i in sorted(nums)]
     print(k) 
     i=k[-1]
@@ -7,6 +9,7 @@ def mxmProduct(nums):
     p = (i-1) * (j-1)
     print(p)
 
+   
 
 mxmProduct([2,1,6,7,3])
 
