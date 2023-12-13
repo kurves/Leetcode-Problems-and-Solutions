b7bd700d9d1dea423daca536fc67cb33ff07a052
kurves <kavssimply@gmail.com>
2023-12-13 22:02:26 +0300

    fix: fix identation error

--- mxmProduct.py
+++ mxmProduct.py
@@ -1,13 +1,7 @@
 def mxmProduct(nums):
     nums.sort()
-        return (nums[-1] - 1) * (nums[-2] - 1)
-    k = [i for i in sorted(nums)]
-    print(k) 
-    i=k[-1]
-    j=k[-2]
-    print(i,j)
-    p = (i-1) * (j-1)
-    print(p)
+    return (nums[-1] - 1) * (nums[-2] - 1)
+   
 
    
 
