882cbf16a69099afc423ec5dcf50267a62f133fa
kurves <kavssimply@gmail.com>
2023-12-15 23:04:10 +0300

    store the items in dict

--- twosum.py
+++ twosum.py
@@ -1,13 +1,18 @@
 def twosum (nums,target):
 
-    #indices={}
+    indices={}
     for i,n in enumerate(nums):
         #for j in range(i+1,len(nums)):
-        if (nums[i] + nums[i]) == target:
-            return [i,i]
+        j = target - n
+        if j in indices:
+            return [indices[j],i]
+        indices[nums] = i    
+        #if (nums[i] + nums[i]) == target:
+           #return [i,i]
+
           
             
-        print([i,i])
+        #print([i,i])
         #print([nums[i], nums[i]])
     
    
