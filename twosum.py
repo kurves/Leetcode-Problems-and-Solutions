d36334754ddf2821c192963a399e1944d6fdff66
kurves <kavssimply@gmail.com>
2023-12-15 22:59:40 +0300

    loop over the array

--- twosum.py
+++ twosum.py
@@ -1,5 +1,7 @@
 def twosum (nums,target):
-    for i in range(len(nums)):
+
+    #indices={}
+    for i,n in enumerate(nums):
         #for j in range(i+1,len(nums)):
         if (nums[i] + nums[i]) == target:
             return [i,i]
