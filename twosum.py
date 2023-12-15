68894d1e82e423167fec28d4e55bb37c1ff9f7ec
kurves <kavssimply@gmail.com>
2023-12-15 22:54:45 +0300

    remove a for loop

--- twosum.py
+++ twosum.py
@@ -1,13 +1,13 @@
 def twosum (nums,target):
     for i in range(len(nums)):
-        for j in range(i+1,len(nums)):
-            if (nums[i] + nums[i]) == target:
-                return [i,i]
+        #for j in range(i+1,len(nums)):
+        if (nums[i] + nums[i]) == target:
+            return [i,i]
           
             
         print([i,i])
-        print([nums[i], nums[j]])
+        #print([nums[i], nums[i]])
     
    
    
-twosum([3,4,6],13)        
+twosum([3,4,6,3],13)        
