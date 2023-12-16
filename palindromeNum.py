0c2cea8a935bc2b58e29b82a806ecdb9b360890d
kurves <kavssimply@gmail.com>
2023-12-16 22:51:37 +0300

    Palindrome num function

--- palindromeNum.py
+++ palindromeNum.py
@@ -2,10 +2,12 @@ def palindromeNum(x):
     y=[]
     for i in str(x):
         y.append(i)
-    if y[:] == y[::-1]:
+        print(i)
+    if y == y[::-1]:
 
         print("True")
     else:
         print('f')
 
-palindromeNum(121)        
+palindromeNum(12)     
+#palindromeNum(9)   
