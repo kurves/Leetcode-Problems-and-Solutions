b3cbcdc433e4332994a5145ddbc6677ddcd2f620
kurves <kavssimply@gmail.com>
2023-12-18 22:45:42 +0300

    function to get common prefix

--- commonPrefix.py
+++ commonPrefix.py
@@ -1,3 +1,11 @@
 def commonPrefix(strs):
     for i in strs:
         print(i)
+        print(i[0][:2])
+        if i[0][:2] == i[1][:2]:
+            print(i[0][:2])
+            print("true")
+
+
+
+commonPrefix(["kurves","kumoh","mwes"])
