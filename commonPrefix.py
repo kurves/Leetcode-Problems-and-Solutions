1b1a27c170994580735e5519893b59433eea947e
kurves <kavssimply@gmail.com>
2023-12-19 22:30:44 +0300

    access individual string elements

--- commonPrefix.py
+++ commonPrefix.py
@@ -1,5 +1,7 @@
 def commonPrefix(strs):
-    [i[0][:2] for i in strs]
+    for i in strs:
+        for j in i:
+            print(j)
     print(i)
     if i[0][:2] == i[1][:2]:
         print(i[0][:2])
