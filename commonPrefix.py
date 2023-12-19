222f7674d004d2b396f789c4c8c515293848c475
kurves <kavssimply@gmail.com>
2023-12-19 22:43:16 +0300

    return the corret output

--- commonPrefix.py
+++ commonPrefix.py
@@ -5,14 +5,14 @@ def commonPrefix(strs):
 
     cp = ""
     for i in range(len(strs)):
-        if i < len(ls) and fs[i] == ls[i]
-        for j in i:
-            print(j)
-    print(i)
-    if i[0][:2] == i[1][:2]:
-        print(i[0][:2])
-        print("true")
+        if i < len(ls) and fs[i] == ls[i]:
+            cp += fs[i]
+        else:
+            break
+    return cp   
 
+    if not strs:
+        return [""]     
 
 
 commonPrefix(["kurves","kumoh","mwes"])
