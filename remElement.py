e30266ccd3eb239495cf3404afa27bd366a7d8b5
kurves <kavssimply@gmail.com>
2023-12-22 23:08:53 +0300

    function to remove element

--- remElement.py
+++ remElement.py
@@ -1,7 +1,10 @@
 def remElement(nums, val):
+    l=0
     for i in range(len(nums)):
-        print(i) 
-        if nums[i] == val:
-            nums.remove(i)
+       
+        if nums[i] != val:
+            nums[l] = nums[i]
+            l+=1
+    return l        
 
 remElement([6,5,4,5,7,9],5)            
