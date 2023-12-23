72ba0280abc6528d9bb5a97b5f25b2635fa9c3dd
kurves <kavssimply@gmail.com>
2023-12-23 23:05:13 +0300

    function to get length of last word

--- wordLength.py
+++ wordLength.py
@@ -1,6 +1,9 @@
 def wordLength(s):
     """Function to get length if last word"""
     s.strip()
+    words = s.plit("")
+    for word in words:
+        print(word)
     lastword=s[-1]
     print(lastword)
     print(len(s))
