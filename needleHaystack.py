3fb667a477bc6b9355daf43565b145132f5af006
kurves <kavssimply@gmail.com>
2023-12-25 22:46:30 +0300

    function to get index

--- needleHaystack.py
+++ needleHaystack.py
@@ -1,6 +1,13 @@
 def needleHaystack(needle, haystack):
+    l= haystack.find(needle)
+    if (l != -1):
+        print(l) 
+    else:
+        return -1    
     if needle in haystack:
         print("true")
+    else:
+        return -1
   
         haystack.split(needle)
        
