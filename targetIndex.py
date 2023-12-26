c233e98c14e4fbb49e8e7890e379ff637095871c
kurves <kavssimply@gmail.com>
2023-12-26 22:13:46 +0300

    function to get target index

--- targetIndex.py
+++ targetIndex.py
@@ -1,5 +1,8 @@
 def targetIndex(nums,target):
     for i,n in enumerate(nums):
         print(i)
-    if nums in target:
-        print(nums)
+        if nums is target:
+            if indexof(nums) == nums[i]:
+                print("yea")
+            print(nums)
+targetIndex([7,5,7],9)
