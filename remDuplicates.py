2b69720d88cf8677b01928c277f9386b8625eabd
kurves <kavssimply@gmail.com>
2024-01-03 22:59:10 +0300

    Fix compilation error

--- remDuplicates.py
+++ remDuplicates.py
@@ -2,14 +2,12 @@ def remDuplicates(nums):
     """function to remiove dupliactes from array"""
     # declare a set to store unique elements
     uniqueNum = 1
-    
-  
    #convert to list to mainatain order
     for i in range(1,len(nums)):
         if nums[i] != nums[i-1]:
             nums[uniqueNum] = nums[i]
             uniqueNum +=1
-    return uniqueNums
+    return uniqueNum
    
 
 remDuplicates([1,1,2])
