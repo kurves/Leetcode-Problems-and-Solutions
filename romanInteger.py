b77304fe703a5c772bd579b138ed38f1bd3bc28b
kurves <kavssimply@gmail.com>
2024-01-06 20:48:36 +0300

    function to convert roman

--- romanInteger.py
+++ romanInteger.py
@@ -1,5 +1,11 @@
 def romanInteger(s):
     Values = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+    result = 0
+    prev_value = 0
+
+    for i in reversed(s):
+        value = Values[i]
+
     dividend =s // 10:
     if dividend == 10:
         print("X")
