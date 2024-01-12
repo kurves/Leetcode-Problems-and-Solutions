b01e92965d969b81dbe62bf90e186e1ac2592846
kurves <kavssimply@gmail.com>
2024-01-12 21:16:46 +0300

    fix compilation error

--- plusOne.py
+++ plusOne.py
@@ -1,8 +1,8 @@
 def plusOne(digits):
     """function to add onr to last digit"""
-    lastIncrease = digits[-1] + 1
+    lastIncrease = [digits[-1] + 1]
     print(lastIncrease)
-    results= digits[:-1] + lastIncrease
+    results= [digits[:-1] , ...lastIncrease]
     print(results)
 
 plusOne([1,2,3])
