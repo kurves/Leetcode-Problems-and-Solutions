d9de33d237d9546f57674a50d0db025ab414b3b5
kurves <kavssimply@gmail.com>
2024-01-13 20:17:36 +0300

    function to add one to last digit

--- plusOne.py
+++ plusOne.py
@@ -1,8 +1,12 @@
 def plusOne(digits):
     """function to add onr to last digit"""
+    rem = 1
+    for i in range(len(digits)-1,-1,-1):
+        total = digits[i] + rem
+        digits[i] = total %10
     lastIncrease = [digits[-1] + 1]
     print(lastIncrease)
     results= digits[:-1] +lastIncrease
     print(results)
 
-plusOne([1,2,3])
+plusOne([9])
