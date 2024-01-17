aa8ed41704ef85c560541465a38af80274ec88e4
kurves <kavssimply@gmail.com>
2024-01-17 21:27:52 +0300

    function to determine if number is happy number

--- happyNumber.py
+++ happyNumber.py
@@ -2,8 +2,8 @@ def happyNumber(n):
     """function  to determine if number is a happy numbetr"""
     
     ind = n % 10
-    inf= n / 10
+    inf= n // 10
 
     print(ind)
     print(inf)
-    
+happyNumber(19)
