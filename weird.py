49d0e351b63ea3483853478ddaa2444231b045b1
kurves <kavssimply@gmail.com>
2024-01-19 19:40:10 +0300

    function to check if number is weird

--- weird.py
+++ weird.py
@@ -1,11 +1,13 @@
 def weird(n):
     even= n % 2 == 0
     odd = n % 2 !=0
-    if n == even and 2 > n < 5:
+    if even and 2 <= n <= 5:
         print("Weird")
-    elif  n == even and 6>n <20:
+    elif  even and 6 <= n <=20:
         print("Weird")
-    elif n == even and n > 20:
+    elif  even and n > 20:
         print("Not Weird")
+    else:
+        print("Weird")
 
 weird(7)
