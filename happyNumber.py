9a30bd647e3315d092ae96cb68c25e3f64cbb45f
kurves <kavssimply@gmail.com>
2024-01-21 22:19:10 +0300

    check if number is happy

--- happyNumber.py
+++ happyNumber.py
@@ -1,8 +1,10 @@
 def happyNumber(n):
     """function  to determine if number is a happy numbetr"""
+    def get_next(num):
+        n = sum(int(digit)**2 for digit in str(n))
     
     seen = set() 
     while n != 1 and n not in seen:
-    seen.add(n) 
-    n = sum(int(digit)**2 for digit in str(n))
+        seen.add(n) 
+   
 happyNumber(19)
