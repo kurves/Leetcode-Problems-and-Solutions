036ae5d79f666a62a2032ff08c3c80806d18b806
kurves <kavssimply@gmail.com>
2024-01-23 21:24:18 +0300

    function to check for isomorpic strings

--- isomorphicStrings.py
+++ isomorphicStrings.py
@@ -9,3 +9,8 @@ def isomorphic(s,t):
         if char_s in mapping_s_t:
             if mapping_s_t[char_s] != char_t:
                 return False
+        else:
+            char_map_s[char_s] = char_t
+        if char_t in char_map_t:
+            if char_map_t[char_t]
+            return False
