0fdf97a01febd55a296acf570752f40cc568389a
kurves <kavssimply@gmail.com>
2024-01-26 20:17:25 +0300

    function to check for single number

--- singleNumber.py
+++ singleNumber.py
@@ -2,6 +2,8 @@ def singleNumber(nums):
     #functiion to find single eements
     items={}
     for item in nums:
+        items.add(item)
+        print(items)
         if item.count == 1:
             print(item)
 
