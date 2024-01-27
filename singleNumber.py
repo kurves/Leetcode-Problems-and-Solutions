e55477576899144095edf79ce90d78fa6aca0c76
kurves <kavssimply@gmail.com>
2024-01-27 19:24:50 +0300

    fix copilation error

--- singleNumber.py
+++ singleNumber.py
@@ -1,8 +1,7 @@
 def singleNumber(nums):
     #functiion to find single eements
     for item in nums:
-        print(items)
-        count = nums.count(item):
-            print(count)
+        count = nums.count(item)
+        print(count)
 
 singleNumber([2,2,1])
