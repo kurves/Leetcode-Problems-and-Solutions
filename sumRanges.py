e9414b098f5b624249d3336e0fa816ade8ca094f
kurves <kavssimply@gmail.com>
2024-01-28 16:50:07 +0300

    function to return smallest sorted list

--- sumRanges.py
+++ sumRanges.py
@@ -7,3 +7,13 @@ def summaryRanges(nums):
     result = []
     start = nums[0]
     end = nums[0]
+
+    
+    for i in range(1, len(nums)):
+    if nums[i] == end + 1:
+        end = nums[i]
+    else:
+        if start == end:
+            result.append(str(start))
+        else:
+            result.append(str(start) + "->" + str(end)
