ad8143bbcbd6eb98646aa34a1689dea05a66d827
kurves <kavssimply@gmail.com>
2024-04-03 14:07:01 +0300

    code to generate pascals triangle

--- pascalTriangle.py
+++ pascalTriangle.py
@@ -9,3 +9,7 @@ def generate(numRows):
             row = [i] * (i +1)
             for j in range(1, i):
                 row[j] = triangle[i - 1][j -1] + triangle[i -1][j]
+            triangle.append(row)
+
+        return triangle
+
